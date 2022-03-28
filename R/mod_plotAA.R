8ffd834ce3cc24cd14905fa143f0e21f4790a67c
CathrineLind <s184338@student.dtu.dk>
2022-03-28 09:44:04 +0000

    Added shiny tags

--- R/mod_plotAA.R
+++ R/mod_plotAA.R
@@ -4,28 +4,44 @@
 #'
 #' @param id,input,output,session Internal parameters for {shiny}.
 #'
-#' @noRd 
+#' @noRd
 #'
-#' @importFrom shiny NS tagList 
+#' @importFrom shiny NS tagList
 mod_plotAA_ui <- function(id){
   ns <- NS(id)
   tagList(
- 
+    shiny::sidebarLayout(
+      shiny::sidebarPanel(
+        shiny::textAreaInput(
+          inputId = ns("peptide"),
+          label = "Peptide sequence",
+          width = 300,
+          height = 100,
+          placeholder = "Insert peptide sequence"
+        )
+      ),
+      shiny::mainPanel(
+        shiny::plotOutput(
+          outputId = ns("abundance")
+        )
+
+      )
+    )
   )
 }
-    
+
 #' plotAA Server Functions
 #'
-#' @noRd 
+#' @noRd
 mod_plotAA_server <- function(id){
   moduleServer( id, function(input, output, session){
     ns <- session$ns
- 
+
   })
 }
-    
+
 ## To be copied in the UI
 # mod_plotAA_ui("plotAA_1")
-    
+
 ## To be copied in the server
 # mod_plotAA_server("plotAA_1")
